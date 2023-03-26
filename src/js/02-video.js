cb76c9cf749e76bc9a9c0e7cfb80c40517bdf719
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-03-26 18:47:55 +0300

    task 2 ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(timeSave, 1000));
+
+function timeSave(e) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(e));
+}
+
+const time = JSON.parse(localStorage.getItem(STORAGE_KEY)).seconds;
+
+player.setCurrentTime(time).then(function(seconds) {
+});
